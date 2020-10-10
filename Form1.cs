115f244c6740718dfacc036c2c416e67a9d1d888
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-10-10 10:12:00 +0300

    Added main structure of the flow of the generation of Data

--- Form1.cs
+++ Form1.cs
@@ -21,13 +21,41 @@ namespace QTLbyRegression
         {
             // Create 3 output files
 
-            //1 table of genotypes
-
-
-            //2 genetic map
-
-
-            //3 table of traits
+           /*  1.table of genotypes: 
+                -tab delimited
+                - columns are individuals, rows are markers
+                -markers have unique name(id, column 0), 
+                -individuals have unique name(id, row 0), can be not provided
+                -only few possible genotypes are possible
+                - some genotypes can be unclear(coded by some text)
+                -some genotypes can be missed(coded by some text)
+                Example:
+                < tab > ind1 < tab > ind2 < tab > ind3
+                m1<tab> AA<tab>AA<tab> AB
+                m2<tab> NA< tab > AA < tab > BB
+                program should read any of such files but the analysis and simulations we will do only for simple case: only four variants for genotypes: -1(missed), 0(AA), 1(AB), 2(BB); all genotypes are clear
+               
+            2.genetic map:
+                -tab delimited
+                - rows are markers(names like in file with genotypes): < marker >< tab >< linkage group(chromosome) >< tab >< coor(cM) >
+                -some markers presented on map are not presented in file with genotypes
+                - some markers with genotypes are not presented on the map
+                Example:
+                marker < tab > chr < tab > coor
+                m1 < tab > Chr1 < tab > 0.1
+                m2 < tab > Chr1 < tab > 56
+              
+            3.table of traits:
+                -tab delimited
+                - usually rows are traits, columns are individuals(can be vice versa)
+                - some traits are unknown
+                - some traits are outliers
+                Example: 
+                trait < tab > ind1 < tab > ind2
+                age < tab > 28 < tab > 45
+                weight < tab > 71.5 < tab > 82
+                height < tab > NA < tab > 180
+           */
         }
     }
 }

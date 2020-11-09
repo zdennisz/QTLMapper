388ccec7afee3093a184cc1cd45b61129588aced
ZeevFrenkel <73228161+ZeevFrenkel@users.noreply.github.com>
2020-11-09 11:23:21 +0200

    Update Chromosome.cs

--- Chromosome.cs
+++ Chromosome.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,13 +8,24 @@ namespace QTLProject
     {
         public int Id { get; set; }//0,1,2,3
         public string Name { get; set; }//for Human: 1,2,3,...,22, X and Y
-        public bool BGender { get; set; }//Human: XX=Girl, XY=Boy
       //  public double LenPhysBp { get; set; }//Physical length of the chromosome (bp=base pairs, 1 kbp=1000 bp, 1Mbp=1,000,000 bp, 1 Gbp=1,000,000,000 bp), (Not important for this project)
         public double LenGenetcM { get; set; }//Genetic length of the chromosome (1 cM ~ 1% of recombination) NB! in this progects equal for males and females
-        public IList<Locus> Locus { get; set; }//list of loci (markers and QTLs)
+
+        public bool BGender { get; set; }//Human: XX=Girl, XY=Boy
         public bool BPresentedInMales { get; set; }//Human: XX=Girl, XY=Boy: => true for all
         public bool BPresentedInFemales { get; set; }//Human: XX=Girl, XY=Boy: => Girl: false for Y chromosome
         public bool BRecInMales { get; set; }//Human: XX=Girl, XY=Boy: Males: false for X and Y
         public bool BRecInFemales { get; set; }//Human: XX=Girl, XY=Boy: Females: true for all
+        
+        public IList<Locus> Locus { get; set; }//list of loci (markers and QTLs)
+
+        public Chromosome()
+        {
+            BGender=false
+            BPresentedInMales=true
+            BPresentedInFemales=true
+            BRecInMales=true
+            BRecInFemales=true
+        }
     }
 }

ce8187aa5fecec18449fda1f3495aea596b515cd
ZeevFrenkel <73228161+ZeevFrenkel@users.noreply.github.com>
2020-11-09 10:43:27 +0200

    Update GenomeOrganization.cs

--- GenomeOrganization.cs
+++ GenomeOrganization.cs
@@ -10,5 +10,11 @@ namespace QTLProject
       
         public MappingIndex MappingFunctionIndex { get; set; }//0=Haldane (independent recombinations), 1=Kossambi, ...(no need more for this project)
         public IList<Chromosome> Chromosome { get; set; }//list of all chromosomes in the genome (Human: includes also X and Y even for Women having no Y chromosome)
+        
+        //number of chromosomes
+        public int nChr()
+        {
+            return(Chromosome.Count)
+        }
     }
 }

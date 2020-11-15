4827e7fcee1b58742e6dc8f4348a1c2eab5358c3
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-11-15 13:04:31 +0200

    Added help button, need to implement the help window and the content of it.

--- Introduction.Designer.cs
+++ Introduction.Designer.cs
@@ -29,129 +29,146 @@
         private void InitializeComponent()
         {
             this.labelIntroText = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.btnHelp = new System.Windows.Forms.Button();
             this.pictureBox9 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // labelIntroText
             // 
             this.labelIntroText.AutoSize = true;
-            this.labelIntroText.Location = new System.Drawing.Point(43, 153);
+            this.labelIntroText.Location = new System.Drawing.Point(14, 153);
             this.labelIntroText.Name = "labelIntroText";
             this.labelIntroText.Size = new System.Drawing.Size(175, 13);
             this.labelIntroText.TabIndex = 0;
             this.labelIntroText.Text = "Hello and Welcome to QTL Mapper";
             // 
-            // pictureBox1
+            // btnHelp
             // 
-            this.pictureBox1.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox1.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox1.Location = new System.Drawing.Point(545, 456);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 112);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox2.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox2.Location = new System.Drawing.Point(463, 342);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(125, 112);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox3.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox3.Location = new System.Drawing.Point(543, 228);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(125, 112);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox4.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox4.Location = new System.Drawing.Point(462, 114);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(125, 112);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox5.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox5.Location = new System.Drawing.Point(542, -1);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(125, 112);
-            this.pictureBox5.TabIndex = 5;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox6.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox6.Location = new System.Drawing.Point(466, 568);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(125, 30);
-            this.pictureBox6.TabIndex = 6;
-            this.pictureBox6.TabStop = false;
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox7.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox7.Location = new System.Drawing.Point(622, 114);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(110, 112);
-            this.pictureBox7.TabIndex = 7;
-            this.pictureBox7.TabStop = false;
-            // 
-            // pictureBox8
-            // 
-            this.pictureBox8.Image = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox8.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox8.Location = new System.Drawing.Point(622, 342);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(110, 112);
-            this.pictureBox8.TabIndex = 8;
-            this.pictureBox8.TabStop = false;
+            this.btnHelp.BackColor = System.Drawing.Color.White;
+            this.btnHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHelp.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Image = global::QTLProject.Properties.Resources.question_mark;
+            this.btnHelp.Location = new System.Drawing.Point(681, 522);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(41, 37);
+            this.btnHelp.TabIndex = 10;
+            this.btnHelp.UseVisualStyleBackColor = false;
             // 
             // pictureBox9
             // 
             this.pictureBox9.Image = global::QTLProject.Properties.Resources.QTLLogo;
             this.pictureBox9.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
-            this.pictureBox9.Location = new System.Drawing.Point(627, 569);
+            this.pictureBox9.Location = new System.Drawing.Point(627, 566);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(105, 32);
+            this.pictureBox9.Size = new System.Drawing.Size(105, 35);
             this.pictureBox9.TabIndex = 9;
             this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox8.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox8.Location = new System.Drawing.Point(622, 339);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(110, 112);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox7.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox7.Location = new System.Drawing.Point(622, 111);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(110, 112);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox6.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox6.Location = new System.Drawing.Point(466, 565);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(125, 36);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox5.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox5.Location = new System.Drawing.Point(542, -4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(125, 112);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox4.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox4.Location = new System.Drawing.Point(462, 111);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(125, 112);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox2.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox2.Location = new System.Drawing.Point(463, 339);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(125, 112);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox1.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(545, 453);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 112);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox3.InitialImage = global::QTLProject.Properties.Resources.QTLLogo;
+            this.pictureBox3.Location = new System.Drawing.Point(543, 225);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(125, 112);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
             // 
             // Introduction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.pictureBox8);
             this.Controls.Add(this.pictureBox7);
@@ -164,15 +181,15 @@
             this.Controls.Add(this.pictureBox3);
             this.Name = "Introduction";
             this.Size = new System.Drawing.Size(732, 601);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,5 +207,6 @@
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.PictureBox pictureBox8;
         private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Button btnHelp;
     }
 }
--- Introduction.cs
+++ Introduction.cs
@@ -16,9 +16,13 @@ namespace QTLProject
         {
             InitializeComponent();
             this.Dock = DockStyle.Fill;
+            this.btnHelp.Click += BtnHelp_Click;
         }
 
-
-
+        private void BtnHelp_Click(object sender, EventArgs e)
+        {
+            //To Do
+            //open help window and  explain what does the app do with screen shots.
+        }
     }
 }

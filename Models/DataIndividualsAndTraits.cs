a29096b35f62a9ba19d37be512129a0e61a3b9a9
ZeevFrenkel <73228161+ZeevFrenkel@users.noreply.github.com>
2020-11-25 09:32:45 +0200

    Update DataIndividualsAndTraits.cs

--- Models/DataIndividualsAndTraits.cs
+++ Models/DataIndividualsAndTraits.cs
@@ -8,11 +8,18 @@ namespace QTLProject
 {
     public class DataIndividualsAndTraits
     {
-        private List<int> genotypeOfInd = new List<int>();
+        //iIndSelected
+        //iLocusSelected
+        //iTraitSelected
         public IList<Locus> Locus { get; set; }//list of selected loci (markers and QTLs)
         public IList<Individ> Individ { get; set; }//list of selected individuals
         public IList<Trait> Trait { get; set; }//list of selected traits 
+        
+        //why do you need these?
+        private List<int> genotypeOfInd = new List<int>();
         public IList<int> GenotypeOfInd { get { return genotypeOfInd; } set { genotypeOfInd = (List<int>)value;} }//[iIndSelected]
+        
+        public int[,] Genotype { get; set; }//[iIndSelected,iLocusSelected]
         public float[,] TraitValue { get; set; }//[iIndSelected,iTraitSelected]
     }
 }

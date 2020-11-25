bc86c3dbf121072f7e2e6d8503fad13b7a1b21d2
ZeevFrenkel <73228161+ZeevFrenkel@users.noreply.github.com>
2020-11-25 12:12:46 +0200

    Update Locus.cs

--- Models/Locus.cs
+++ Models/Locus.cs
@@ -18,8 +18,19 @@ namespace QTLProject
         public Position Position { get; set; }//ID of chromosome
         public List<QTL_SingleLocusEffectOnSingleTrait>  QTL = new List<QTL_SingleLocusEffectOnSingleTrait>() ;
        
-        public IList<AllelName> AlleneNameOriginal { get { return alleneNameOriginal;  } set { alleneNameOriginal = (List<AllelName>)value; } }//usually ('0','1'), ('a','A'), ('m','M'), ('A','T'), ('C','G'),... 
+        //public IList<AllelName> AlleneNameOriginal { get { return alleneNameOriginal;  } set { alleneNameOriginal = (List<AllelName>)value; } }//usually ('0','1'), ('a','A'), ('m','M'), ('A','T'), ('C','G'),... 
+        public string[] AlleneNameOriginal=new string[2]
         public LocusDominanceStatus IdDominanceStatus { get; set; }//0 - allele 0 is dominant, 1 - allele 1 is dominant, 2 - codominant
+        
+        public Locus(int nLociCurrent, Position pos){
+            Id=nLociCurrent
+            Position=pos
+            Name="m"+Id.ToString()+"_"+pos.Chromosome.Name+"_"+pos.PositionChrGenetic.ToString()//m1234_X_15.5
+            BMarker=true
+            QTL = new List<QTL_SingleLocusEffectOnSingleTrait>() ;
+            IdDominanceStatus=LocusDominanceStatus.IncompleteDominance;//1, i.e., codominant
+            AlleneNameOriginal={"A", "B"};
+        }
 
     }
 }

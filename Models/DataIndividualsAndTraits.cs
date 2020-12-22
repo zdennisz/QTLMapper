119be54051e32033a3f7e559e23540809fa2ef42
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2020-12-22 17:19:27 +0200

    Removed irrelevant list

--- Models/DataIndividualsAndTraits.cs
+++ Models/DataIndividualsAndTraits.cs
@@ -19,11 +19,7 @@ namespace QTLProject
         public IList<Locus> Locus { get { return locus; } set { locus = (List<Locus>)value; } }//list of selected loci (markers and QTLs)
         public IList<Individ> Individ { get { return individ; } set { individ = (List<Individ>)value; } }//list of selected individuals
         public IList<Trait> Trait { get { return trait; } set { trait = (List<Trait>)value; } }//list of selected traits 
-        
-        //why do you need these?
-        private List<int> genotypeOfInd = new List<int>();
-        public IList<int> GenotypeOfInd { get { return genotypeOfInd; } set { genotypeOfInd = (List<int>)value;} }//[iIndSelected]
-        
+         
         public int[,] Genotype { get; set; }//[iIndSelected,iLocusSelected]
         public bool[,] GenotypeOk { get; set; }//[iIndSelected,iLocusSelected]
         public float[,] TraitValue { get; set; }//[iIndSelected,iTraitSelected]

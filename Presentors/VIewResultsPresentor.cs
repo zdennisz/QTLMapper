db5eae6b29fe481ac478c231665791fd51a5e349
עודד פישר <odedf2323@gmail.com>
2020-12-27 20:53:04 +0200

    ttest continue

--- Presentors/VIewResultsPresentor.cs
+++ Presentors/VIewResultsPresentor.cs
@@ -41,7 +41,7 @@ namespace QTLProject
             HashSet<int> indi_1 = new HashSet<int>(); // individuals with genotype 1
             int n0, n1; // should it be int? (size of sets) ?
             double x_i, y_i, x_a, y_a, x_s2 = 0.0, y_s2 = 0.0; // trait values, average and variance of individuals sets
-            double x_sum = 0, y_sum = 0;
+            double x_sum = 0, y_sum = 0, x_sum_variance=0, y_sum_variance=0;
             double ttest;
 
 
@@ -84,7 +84,27 @@ namespace QTLProject
 
             x_a = x_sum / n0; // average of set with genotype 0 
             y_a = y_sum / n1; // average of set with genotype 1
+            foreach (int i in indi_0)
+            {
+                for (int j = 0; j < this.db.SubData[i].TraitValue.Length; j++)
+                {
+                    x_sum_variance += Math.Pow((db.SubData[i].TraitValue[0, j] - x_a), 2);
+                }
+            }
+
+            foreach (int i in indi_1)
+            {
+                for (int j = 0; j < this.db.SubData[i].TraitValue.Length; j++)
+                {
+                    y_sum_variance += Math.Pow((db.SubData[i].TraitValue[0, j] - x_a), 2);
+                }
+            }
+            x_s2 = x_sum_variance / n0; // the variance of indi_0
+            y_s2 = y_sum_variance / n1; // the variance of indi_1
+
             ttest = (y_a - x_a) / (Math.Sqrt(y_s2 / Math.Sqrt(n1) + x_s2 / Math.Sqrt(n0)));
+
+               
 
             return Convert.ToString(result);
         }
